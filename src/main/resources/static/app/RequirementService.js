207ae89d39beb894f894a8bda77b74e274fe64e7
Marcelo <marcelo.neres1@gmail.com>
2016-01-04 17:12:43 -0200

    Fixed pages for add new goals

--- src/main/resources/static/app/RequirementService.js
+++
@@ -1,8 +0,0 @@
-(function(angular) {
-  var RequirementFactory = function($resource) {
-    return $resource('/api/goals/addRequirement');
-  };
-  
-  RequirementFactory.$inject = ['$resource'];
-  angular.module("myApp.services").factory("RequirementFactory", RequirementFactory);
-}(angular));
